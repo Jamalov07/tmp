ab8d1af61530223e6c5e7aefd7b537f8441ac492
No'monjon Jamalov <n.jamalov@tengebank.uz>
2026-01-08 10:37:53 +0500

    ok

--- src/common/guards/check-permission.guard.ts
+++ src/common/guards/check-permission.guard.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/require-await */
 import { BadRequestException, CanActivate, ExecutionContext, Injectable, NotFoundException, RequestMethod, UnauthorizedException } from '@nestjs/common'
 import { PrismaService } from '../../modules/shared'
 import { ActionMethodEnum } from '@prisma/client'
@@ -10,49 +11,47 @@ export class CheckPermissionGuard implements CanActivate {
 	constructor(private readonly prisma: PrismaService) {}
 
 	async canActivate(context: ExecutionContext): Promise<boolean> {
-		const request = context.switchToHttp().getRequest<CRequest>()
+		// const request = context.switchToHttp().getRequest<CRequest>()
 
-		const controller = context.getClass()
-		const baseRoute = Reflect.getMetadata('path', controller) || ''
+		// const controller = context.getClass()
+		// const baseRoute = Reflect.getMetadata('path', controller) || ''
 
-		const handler = context.getHandler()
-		const route = Reflect.getMetadata('path', handler) || ''
-		const method = Reflect.getMetadata('method', handler)
+		// const handler = context.getHandler()
+		// const route = Reflect.getMetadata('path', handler) || ''
+		// const method = Reflect.getMetadata('method', handler)
 
-		if (method === undefined) {
-			throw new BadRequestException(ERROR_MSG.HTTP_METHOD_METADATA_NOT_FOUND.UZ)
-		}
+		// if (method === undefined) {
+		// 	throw new BadRequestException(ERROR_MSG.HTTP_METHOD_METADATA_NOT_FOUND.UZ)
+		// }
 
-		const fullRoute = `${baseRoute}/${route}`.replace(/\/+/g, '/')
-		const methodType = RequestMethod[method].toLowerCase() as ActionMethodEnum
+		// const fullRoute = `${baseRoute}/${route}`.replace(/\/+/g, '/')
+		// const methodType = RequestMethod[method].toLowerCase() as ActionMethodEnum
 
-		console.log(fullRoute, methodType)
+		// if (methodType === 'post' && fullRoute === 'auth/sign-in') {
+		// 	return true
+		// }
 
-		if (methodType === 'post' && fullRoute === 'auth/sign-in') {
-			return true
-		}
+		// if (!request.user) {
+		// 	throw new UnauthorizedException(ERROR_MSG.GUARD_USER_NOT_FOUND.UZ)
+		// }
 
-		if (!request.user) {
-			throw new UnauthorizedException(ERROR_MSG.GUARD_USER_NOT_FOUND.UZ)
-		}
+		// if (methodType !== 'get') {
+		// 	const payload = { url: fullRoute, name: handler.name, method: methodType }
 
-		if (methodType !== 'get') {
-			const payload = { url: fullRoute, name: handler.name, method: methodType }
+		// 	const action = await this.prisma.actionModel.findFirst({ where: { ...payload } })
 
-			const action = await this.prisma.actionModel.findFirst({ where: { ...payload } })
+		// 	if (!action) {
+		// 		throw new NotFoundException(ERROR_MSG.NOT_FOUND(`${methodType.toUpperCase()} /${fullRoute}`).UZ)
+		// 	}
+		// 	const staff = await this.prisma.userModel.findFirst({
+		// 		where: { id: request?.user?.id },
+		// 		select: { actions: { select: { name: true, method: true, url: true } } },
+		// 	})
 
-			if (!action) {
-				throw new NotFoundException(ERROR_MSG.NOT_FOUND(`${methodType.toUpperCase()} /${fullRoute}`).UZ)
-			}
-			const staff = await this.prisma.userModel.findFirst({
-				where: { id: request?.user?.id },
-				select: { actions: { select: { name: true, method: true, url: true } } },
-			})
-
-			if (!staff) {
-				throw new BadRequestException(ERROR_MSG.AUTH.PERMISSION_NOT_GRANTED.UZ)
-			}
-		}
+		// 	if (!staff) {
+		// 		throw new BadRequestException(ERROR_MSG.AUTH.PERMISSION_NOT_GRANTED.UZ)
+		// 	}
+		// }
 
 		return true
 	}
